78a6ad2771fab10f813abe97ba1e04eefcfd10e3
newmar.souza <newmar.souza@visie.com.br>
2024-08-11 17:04:35 -0400

    :recycle: refactor: ajustando posições dos itens na seção de favoritos

--- src/components/Sections/Favorites/index.tsx
+++ src/components/Sections/Favorites/index.tsx
@@ -10,19 +10,19 @@ export function Favorites({ favoriteHooks }: Props) {
   const { favorites } = favoriteHooks
 
   return (
-    <div className='py-4 container h-[calc(100%-80px)] sm:h-full'>
-      {!!favorites?.length ? (
-        <div className='h-full overflow-hidden'>
-          <h1 className='text-primary mb-6 text-center'>Meus favoritos</h1>
+    <div className='py-4 w-full h-[calc(100%-80px)] sm:h-full max-w-4xl mx-auto'>
+      <div className='h-full overflow-hidden'>
+        <h1 className='text-primary mb-6 text-center'>Meus favoritos</h1>
+        {!!favorites?.length ? (
           <section className='flex flex-col gap-4 h-[calc(100%-55px)] overflow-y-auto pr-2'>
             {favorites.map(repo => (
               <RepoCard key={repo.id} favoriteHooks={favoriteHooks} repo={repo} />
             ))}
           </section>
-        </div>
-      ) : (
-        <p>Nenhum favorito salvo</p>
-      )}
+        ) : (
+          <p>Nenhum favorito salvo</p>
+        )}
+      </div>
     </div>
   )
 }
